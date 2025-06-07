b9a05b3a803820b2139a2e6f59230b68a838b4fd
AlexiaPT <apalaciost@pucp.edu.pe>
2025-06-06 21:22:14 -0500

    ProveedorService

--- src/main/java/com/example/lab7_2021049/service/ProveedorService.java
+++ src/main/java/com/example/lab7_2021049/service/ProveedorService.java
@@ -1,4 +1,13 @@
 package com.example.lab7_2021049.service;
 
-public class ProveedorService {
+import com.example.lab7_2021049.entity.Proveedor;
+
+import java.util.List;
+
+public interface ProveedorService {
+    Proveedor crearProveedor(Proveedor p);
+    Proveedor obtenerProveedor(Long id);
+    List<Proveedor> listarProveedores();
+    Proveedor actualizarProveedor(Long id, Proveedor p);
+    void eliminarProveedor(Long id);
 }
